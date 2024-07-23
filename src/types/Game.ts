acaeddf3105452ced560fea95b7879fb459c3fda
Gusttavo Ruy <7leckler@gmail.com>
2024-07-23 09:24:05 -0300

    feat: novos tipos

---
+++ src/types/Game.ts
@@ -0,0 +1,22 @@
+import { BattleType, ProfileType } from ".";
+
+export interface GameFields {
+    // [0] = User; [1] = Enemy;
+    turn: 0 | 1;
+    players: [ProfileType.default, ProfileType.default];
+    battle: BattleType.default;
+}
+
+export interface GameMethods {
+    actualTurn(): 0 | 1
+
+    nextTurn(): 0 | 1
+
+    toggleTurn(): void
+
+    resetLifes(): void
+
+    playRound(): void
+}
+
+export default interface Game extends GameFields, GameMethods { }
